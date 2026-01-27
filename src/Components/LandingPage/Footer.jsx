66f0b72b07fb7d35082ccaadf96939441da4320e
Danielazan <danfrancix@gmail.com>
2026-01-27 14:25:40 +0100

    Landinf page almost done

--- src/Components/LandingPage/Footer.jsx
+++ src/Components/LandingPage/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import lingImg from "../../assets/textbg.png"
+import btnImg from "../../assets/textbg.png"
 import Logo from "../../assets/Logo.png"
 import { FaYoutube, FaEnvelope, FaTwitter, FaFacebook, FaInstagram, FaLinkedin, FaTiktok } from 'react-icons/fa';
 import { Twitter } from 'lucide-react';
@@ -7,7 +8,7 @@ import { Twitter } from 'lucide-react';
 
 const Footer = () => {
     return (
-        <div className="min-h-screen w-full bg-cover bg-center bg-no-repeat bg-[#000000]  relative">
+        <div className="h-100 w-full bg-cover bg-center bg-no-repeat bg-[#000000]  relative">
 
             <div className="w-full px-2 md:px-5 relative overflow-hidden ">
                 {/* Links section1 */}
@@ -28,8 +29,8 @@ const Footer = () => {
                         <span>Nnenna Eloka</span>
                     </div>
 
-                    <div className=" col-span-1 px-2 py-6 text-white flex gap-5 ">
-                        <FaYoutube className="text-2xl" />
+                    <div className=" col-span-1 px-2 py-6 text-white flex gap-5 items-start">
+                        <FaYoutube className="text-6xl" />
                         <span className='text-sm'>https:/youtube.com/@nnenna-eloka?si=ggmMsxl1K6X99AWC</span>
                     </div>
 
@@ -49,7 +50,7 @@ const Footer = () => {
                             alt="Absolutely positioned image"
                             className=" w-12 h-12 object-cover rounded-lg shadow-lg"
                         />
-                        <div className='w-200 flex flex-col bg-red-800'>
+                        <div className='w-200 flex flex-col '>
                             <p className='font-poppins text-sm font-light text-white leading-8 text-left'>
                                 Nnenna Eloka is a businesswoman, media personality and founder focused on building impactful ideas and platforms.
                             </p>
@@ -63,6 +64,45 @@ const Footer = () => {
                                     <FaLinkedin className="text-2xl" />
                                     <Twitter className="text-2xl" />
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+
+                        {/* Items 2 */}
+                    <div className='flex flex-col text-white'>
+                        <h1 className='font-poppins'>Quick Links</h1>
+
+                        <div className='flex flex-col mt-6 font-poppins text-white'>
+                            <h4>Home</h4>
+                            <h4>About Nnenna</h4>
+                            <h4>The Survival Game</h4>
+                            <h4>Foundation</h4>
+                            <h4>Businesses</h4>
+                        </div>
+                    </div>
+                    {/* item 3 */}
+                    <div className='flex flex-col text-white'>
+                        <h1 className='font-poppins'>Resources</h1>
+
+                        <div className='flex flex-col mt-6 font-poppins text-white'>
+                            <h4>Blog</h4>
+                            <h4>Newsletter</h4>
+                            <h4>Privacy Policy</h4>
+                        </div>
+                    </div>
+
+
+                    {/* Item 4 */}
+
+                     <div className='flex flex-col text-white'>
+                        <h1 className='font-poppins'>Subscribe</h1>
+
+                        <div className='w-full flex flex-col mt-6 font-poppins text-white gap-2'>
+                            <h4>Get more updates straight to your inbox</h4>
+                            <h4>Newsletter</h4>
+                            <div className='border rounded-2xl border-[#F0AE12] flex  items-center justify-between w-5/6 h-10'>
+                                <h1 className='font-poppins text-l pl-4'>Enter your email here</h1>
+                                <button className='w-18 rounded-r-2xl h-full text-xs text-black font-bold' style={{backgroundImage:`url(${btnImg})`}}>Subscribe</button>
                             </div>
                         </div>
                     </div>
